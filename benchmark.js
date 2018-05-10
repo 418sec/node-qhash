aa409c490536620bdbc15f613bf9ba3417a6d225
Andras <andras@kinvey.com>
2018-05-10 00:08:29 -0400

    more benchmark cases

--- benchmark.js
+++ benchmark.js
@@ -1,5 +1,6 @@
 qtimeit = require('qtimeit');
 qhash = require('./');
+util = require('util');
 
 qtimeit.bench.timeGoal = 0.40;
 qtimeit.bench.visualize = true;
@@ -8,12 +9,32 @@ var x;
 var hash = {a: {b: {c: 0}}};
 var hash2 = {a: {b: 0}};
 var hash5 = {a: {b: {c: {d: {e: 0}}}}};
+var arr4 = [ {a:{b:1}}, {a:{b:2}}, {a:{b:3}}, {a:{b:4}} ];
 var hash2L = {firstFieldWithVeryLongName: {secondFieldWithVeryLongName: 1}};
 var hash2M = {firstFieldName: {secondFieldName: 1}};
 var hash1M = {firstFieldName: 1};
 var hash2S = {firstName: {secondName: 1}};
-var h0 = {}, h1 = {a:1}
+var h0 = {}, h1 = {a:1}, h5 = {a:{b:{c:{d:{e:123}}}}}
+var hfoo = {foo:1}
 qtimeit.bench({
+    'merge {}, {a:1}': function(){
+        x = qhash.merge({}, h1);
+    },
+    'merge {}, {foo:1}': function(){
+        x = qhash.merge({}, hfoo);
+    },
+    'merge {}, {a:{b:{c:{d:{e:123}}}}}': function(){
+        x = qhash.merge({}, h5);
+    },
+    'selectField a': function(){
+        x = qhash.selectField(arr4, 'a');
+    },
+    'selectField a.b': function(){
+        x = qhash.selectField(arr4, 'a.b');
+    },
+})
+console.log("")
+if (1) qtimeit.bench({
     'set x': function(){
         qhash.set(hash, 'x', 1);
     },
@@ -80,5 +101,12 @@ qtimeit.bench({
 
     'merge {}, {a:1}': function(){
         x = qhash.merge({}, h1);
+    },
+
+    'util._extend {}, {a:1}': function(){
+        x = util._extend({}, h1);
+    },
+    'Object.assign {}, {a:1}': function(){
+        x = Object.assign({}, h1);
     },
 })
